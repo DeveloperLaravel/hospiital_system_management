4e80ccb334c6b5e10c034909e317bf85460601e5
Abbes <abbes.dev@gmail.com>
2026-02-16 16:40:05 +0200

    route off

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('users', UserController::class)->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('users', UserController::class);
 });
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->group(function () {});
 require __DIR__.'/auth.php';
